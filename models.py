a6fd6d5c5e24c67bc3d7b79d269b3a15a0396753
Tyler Walker <Tyler Walker>
2015-08-12 12:16:02 -0700

    Added testing functionality.

--- models.py
+++ models.py
@@ -1,23 +1,25 @@
-from psycopg2 import * # Imports all of MySQL
+import sqlite3 as sql
+import test_model
 
 
-def user():
+def user(cursor):
     # A user has many accounts
-
-    #TODO Create User Table with Columns (account_id, first_name, last_name, SSN)
     # CREATE TABLE USER (account_id int NOT NULL, first_name varchar(100), last_name var_char(100), SSN, varchar(9), PRIMARY KEY(account_id))
+    cursor.execute("""CREATE TABLE users (account_id int NOT NULL, first_name CHAR(100), last_name CHAR(100), SSN CHAR(9), PRIMARY KEY(account_id));""")
     return
 
 
-def account():
+def account(cursor):
     # An account belongs to a user
     # An account has many transactions
 
-    #TODO Create Account Table with Columns (account_id, nick_name, interest_rate)
+    cursor.execute("""CREATE TABLE accounts (account_id int NOT NULL, nickname CHAR(100), interest_rate DECIMAL(10,5));""")
+    if test_model.table_exists(cursor, "accounts"):
+        print("Accounts Exists.")
     return
 
 
-def transaction():
+def transaction(cursor):
     # A transaction belongs to an account
 
     #TODO Create Account Table with Columns (account_id, transaction_id, amount, credit/debit)
@@ -27,9 +29,28 @@ def transaction():
 def main():
 
     try:
-        conn = psycopg2.connect("dbname='banking' user='bankadmin' host='localhost' password='dbpass123'")
+        conn = sql.connect('bank.db')
+        cur = conn.cursor()
     except:
+        print("Can't connect to the database.")
 
-    #check if tables exist
-    if
+    # Create Users table.
+    if not test_model.table_exists(cur, "users"):
+        user(cur)
+    else:
+        if test_model.table_exists(cursor, "users"):
+            print("Users Exists.")
+
+    # Create Accounts table. 
+    if not test_model.table_exists(cur, "accounts"):
+        account(cur)
+    else:
+        if test_model.table_exists(cursor, "accounts"):
+            print("Accounts Exists.")
+
+    if conn:
+        conn.close()
     return
+
+if __name__ == '__main__':
+    main()
