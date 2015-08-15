62c7d26c93ef85882adead2913113abf5eeadb10
Tyler Walker <Tyler Walker>
2015-08-14 17:58:41 -0700

    Updating Accounts and Users

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 import sqlite3 as sql
 import test_model
-
+import seed
 
 def user(cursor):
     # A user has many accounts
     # CREATE TABLE USER (account_id int NOT NULL, first_name varchar(100), last_name var_char(100), SSN, varchar(9), PRIMARY KEY(account_id))
-    cursor.execute("""CREATE TABLE users (account_id int NOT NULL AUTO_INCREMENT, first_name CHAR(100), last_name CHAR(100), SSN CHAR(9), PRIMARY KEY(account_id));""")
+    cursor.execute("""CREATE TABLE users (user_id INTEGER PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(100), last_name VARCHAR(100), SSN VARCHAR(11));""")
+    if test_model.table_exists(cursor, "users"):
+        print("Users Exists.")
     return
 
 
@@ -13,7 +15,7 @@ def account(cursor):
     # An account belongs to a user
     # An account has many transactions
 
-    cursor.execute("""CREATE TABLE accounts (account_id int NOT NULL, nickname CHAR(100), interest_rate DECIMAL(10,5));""")
+    cursor.execute("""CREATE TABLE accounts (account_id INTEGER PRIMARY KEY, user_id INTEGER, nickname CHAR(100), interest_rate DECIMAL(10,5));""")
     if test_model.table_exists(cursor, "accounts"):
         print("Accounts Exists.")
     return
@@ -42,13 +44,6 @@ def main():
             print("Users Exists.")
 
     # Create Accounts table.
-    if not test_model.table_exists(cur, "accounts"):
-        account(cur)
-    else:
-        if test_model.table_exists(cur, "accounts"):
-            print("Accounts Exists.")
-
-    # Create Transactions table.
     if not test_model.table_exists(cur, "accounts"):
         account(cur)
     else:
--- seed.py
+++ seed.py
@@ -1,4 +1,5 @@
 import random
+import sqlite3 as sql
 
 first_names = ["Tyler", "Jason", "James", "Alice", "Walter", "Grace", "Ada",
                "Erin", "Jacob", "Des", "Heather", "Henry", "Beth", "Emily", "Joe"]
@@ -9,23 +10,26 @@ last_names = ["Walker", "Swanby", "Mullen", "Johnson", "Bookwalter", "Hopper", "
                "Stephens", "Hall", "Lewis", "King", "Jackson"]
 
 
+def SSN_print(ssn):
+    return ssn[0:3] + "-" + ssn[3:5] + "-" + ssn[5:]
+
+
 def drop_table(cursor, table):
-    cursor.execute("DELETE FROM %s *" %s table)
+    cursor.execute("DELETE FROM %s *" % table)
 
 
 def users_seed(cursor):
-    names = []
+    people = []
     for i in range(1000):
         first_name = first_names[random.randint(0,len(first_names) - 1)] # Random First Name
         last_name  = last_names[random.randint(0,len(last_names) - 1 )]   # Random Last Name
+        SSN = ""
+        for j in range(9):
+            SSN += str(random.randint(0,9))
+        SSN = SSN_print(SSN)
+        people.append((first_name, last_name, SSN))
 
-        names.append((first_name, last_name))
-    for first, last in names:
-        cursor.execute("""INSERT INTO users
-                          (first_name, last_name, SSN)
-                          VALUES (?,?,?)
-                       """, (first_name, last_name, SSN))
-
+    cursor.executemany('INSERT INTO users(first_name,last_name,SSN) VALUES (?,?,?)', people)
     return
 
 
@@ -36,4 +40,20 @@ def accounts_seed():
 def transactions_seed():
     return
 
-users_seed()
+
+def main():
+    try:
+        conn = sql.connect('bank.db')
+        cur = conn.cursor()
+    except:
+        print("Can't connect to the database.")
+
+    users_seed(cur)
+
+    conn.commit()
+    if conn:
+        conn.close()
+    return
+
+if __name__ == "__main__":
+    main()
